cc9fd96994913034b19a18c8845f3753361e573d
xiahu <xiahu@clinbrain.com>
2021-11-02 15:48:20 +0800

    flink-cdc

--- flink-cdc/src/main/java/cn/xhjava/flink/cdc/PostgresCdcTable.java
+++ flink-cdc/src/main/java/cn/xhjava/flink/cdc/PostgresCdcTable.java
@@ -1,53 +1,41 @@
 package cn.xhjava.flink.cdc;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
-import org.apache.flink.types.Row;
 
 /**
  * @author Xiahu
  * @create 2021/10/27 0027
  */
 public class PostgresCdcTable {
-    public static void main(String[] args) {
-        //设置flink表环境变量
-        EnvironmentSettings fsSettings = EnvironmentSettings.newInstance()
-                .useBlinkPlanner()
-                .inStreamingMode()
-                .build();
-
-        //获取flink流环境变量
-        StreamExecutionEnvironment exeEnv = StreamExecutionEnvironment.getExecutionEnvironment();
-        exeEnv.setParallelism(1);
-
-        //表执行环境
-        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(exeEnv, fsSettings);
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
         //拼接souceDLL
-        String sourceDDL = "CREATE TABLE shipments (\n" +
-                "  id INT,\n" +
-                "  age INT,\n" +
-                "  name STRING\n" +
-                ") WITH (\n" +
-                "  'connector' = 'postgres-cdc',\n" +
-                "  'hostname' = '192.168.119.128',\n" +
-                "  'port' = '5432',\n" +
-                "  'username' = 'postgres',\n" +
-                "  'password' = 'postgres',\n" +
-                "  'database-name' = 'postgres',\n" +
-                "  'schema-name' = 'public',\n" +
-                "  'table-name' = 'cdc_pg_source'\n" +
+        String sourceDDL = "CREATE TABLE shipments (" +
+                "  id INT," +
+                "  age INT," +
+                "  name STRING" +
+                ") WITH (" +
+                "  'connector' = 'postgres-cdc'," +
+                "  'hostname' = '192.168.119.128'," +
+                "  'port' = '5432'," +
+                "  'username' = 'postgres'," +
+                "  'password' = 'postgres'," +
+                "  'decoding.plugin.name' = 'wal2json'," +
+                "  'database-name' = 'postgres'," +
+                "  'schema-name' = 'public'," +
+                "  'table-name' = 'cdc_pg_source'" +
                 ")";
 
 
         //执行source表ddl
         tableEnv.executeSql(sourceDDL);
+        tableEnv.executeSql("SELECT * FROM shipments").print();
 
-        String query = "SELECT * FROM shipments";
-        Table table = tableEnv.sqlQuery(query);
 
-        tableEnv.toRetractStream(table, Row.class);
+        env.execute();
     }
 }
